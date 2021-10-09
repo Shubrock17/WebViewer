238390bba49591f7a43c49d44b1d7535e4ec0709
Shubhankar <44376659+Shubrock17@users.noreply.github.com>
2021-10-09 15:40:36 +0530

    Home Page UI

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,55 +3,95 @@ import Pdftron from "./Pdftron";
 import axios from "axios";
 import { auth } from "../Config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import SearchBar from "./Search";
+ 
 const Home = () => {
+  const filterPosts = (posts, query) => {
+    if (!query) {
+      return posts;
+    }
+
+    return posts.filter((post) => {
+      const postName = post.name.toLowerCase();
+      return postName.includes(query);
+    });
+  };
+
   const [user] = useAuthState(auth);
   const [filelist, setfilelist] = useState(null);
   const [fileset, setfileset] = useState();
   const [boolean, setboolean] = useState(false);
-  const deletefile=(item)=>{
-    axios.delete(`http://localhost:5000/ppt/${item}`).then((resp)=>{
-      callbackend();
-    }).catch((err)=>console.log(err));
-  }
-  const callbackend=()=>{
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get("s");
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const filteredPosts = filterPosts(filelist, searchQuery);
+
+  const deletefile = (item) => {
     axios
-    .get(`http://localhost:5000/ppt/user/${user._delegate.email}`)
-    .then((resp) => {
-      setfilelist(resp.data);
-    })
-    .catch((err) => console.log(err));
-  }
+      .delete(`http://localhost:5000/ppt/${item}`)
+      .then((resp) => {
+        callbackend();
+      })
+      .catch((err) => console.log(err));
+  };
+  const callbackend = () => {
+    axios
+      .get(`http://localhost:5000/ppt/user/${user._delegate.email}`)
+      .then((resp) => {
+        setfilelist(resp.data);
+      })
+      .catch((err) => console.log(err));
+  };
   useEffect(() => {
-   callbackend();
+    callbackend();
   }, [user]);
   const showViewer = (value) => {
     setboolean(true);
     setfileset(value);
   };
+
   const DisplayImagesFromContainer = () => (
     <div>
-      
-      <h1 style={{fontFamily:"georgia",display: "flex",flexDirection: "column",justifyContent: "center",textAlign: "center",height:"0px"}} >List of my files</h1>
-      <ul style={{ display: "block" }}>
-        {filelist.map((item) => {
+      <h1
+        style={{
+          fontFamily: "georgia",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          height: "0vh",
+        }}
+      >
+        List of my files
+      </h1>
+      <div style={{marginLeft:"2.5%",padding:"1%",marginBottom:"2%"}}>
+          <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+      </div>
+      <ul style={{ display: "block",fontSize: "1.0em",fontWeight:"normal"}}>
+        {filteredPosts.map((post) => {
           return (
             <>
-              <li style={{listStyle:"square"}}>
-                <div style={{ display: "flex" }}>
-                  <div
+              <li style={{ listStyle: "square" ,paddingTop: "12px",paddingBottom: "0px"}} key={post.key}>
+                {" "}
+                <div >
+                  <div style={{ display: "inline" ,width:"35%",float:"left"}}
                     onClick={(event) => {
-                      console.log(item.pdfurl);
-                      showViewer(item.pdfurl);
+                      console.log(post.pdfurl);
+                      showViewer(post.pdfurl);
                     }}
                   >
-                    {item.name}
-                  </div>
-                  <div >
-                  <FontAwesomeIcon icon={faTrashAlt} onClick={(event) => {
-                      deletefile(item.name);
-                    }} />
+                    {post.name}
+                  </div>{" "}
+                  <div>
+                    {/* <button style={{marginLeft:"10%"}}>Delete</button> */}
+                    <FontAwesomeIcon
+                      icon={faTrashAlt}
+                      onClick={(event) => {
+                        deletefile(post.name);
+                      }}
+                    />
                   </div>
                 </div>
               </li>
@@ -64,12 +104,11 @@ const Home = () => {
 
   return (
     <>
-      
       {!boolean && filelist && DisplayImagesFromContainer()}
       {boolean && fileset && (
-          <div>
-            <Pdftron url={fileset} />
-          </div>
+        <div>
+          <Pdftron url={fileset} />
+        </div>
       )}
     </>
   );
---
+++ src/Components/Search.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => (
+    <form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search Files</span>
+        </label>
+        <input
+          value={searchQuery}
+          onInput={e => setSearchQuery(e.target.value)}
+            type="text"
+            id="header-search"
+            placeholder="Search files"
+            name="s" 
+        />
+        <button type="submit">Search</button>
+    </form>
+);
+
+export default SearchBar;
