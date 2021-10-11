30b486a8ef631fad33576463f9d1b032a123fece
Khushi Balodi <khushibalodi@gmail.com>
2021-10-11 10:57:02 +0530

    Update CommentBox.js

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -3,24 +3,24 @@ import axios from "axios";
 import CommentForm from "./CommentForm";
 import Comment from "./Comment";
 import "./Comments.scss";
-import jsPDF from "jspdf";
-
 //Comment box for our view
 const CommentBox = ({ pptname, pageNumber }) => {
   const [showComments, setshowComments] = useState(false);
-  const [comments, setcomments] = useState([
-    {
-      id: 1,
-      author: "khushi",
-      body: "sample comment",
-      slideid: 1,
-    },
-  ]);
+  const [comments, setcomments] = useState([]);
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/ppt/${pptname}`)
+      .get(`http://localhost:5000/ppt/${pptname}/comments`)
       .then((resp) => {
-        console.log(resp);
+        console.log(resp.data);
+        resp.data.map((comment) => {
+          let mycomm = {
+            id: comment.id,
+            author: comment.author,
+            body: comment.comment,
+            slideid: comment.slideid,
+          };
+          setcomments((comments) => comments.concat(mycomm));
+        });
       })
       .catch((err) => console.log(err));
   }, [pptname]);
@@ -41,37 +41,13 @@ const CommentBox = ({ pptname, pageNumber }) => {
     setcomments((comments) => comments.concat(comment));
     axios
       .post(`http://localhost:5000/ppt/${pptname}/comments`, bodytosend)
-      .then((resp) => {})
+      .then((resp) => {
+        console.log(resp);
+      })
       .catch((err) => console.log(err));
   };
   const _handleClick = () => {
     setshowComments(!showComments);
-  };
-  const test = () => {
-    axios.get("http://localhost:5000/ppt/Final_Idea Submission Template_Hackpions edition 3_8d458c00e4f07f.pptx/comments").then((resp)=>{
-    var obj;
-    obj = resp.data;
-    console.log(resp.data[0]);
-    var doc = new jsPDF("p", "pt");
-    doc.text(250, 20, "Comments");
-    let x=20;
-    let y=60;
-    obj.map((comment) => {
-      doc.text(x, y, `Comment : ${comment.comment}`);
-      y+=20;
-      doc.text(x, y, `Author : ${comment.author}`);
-      y+=20;
-      doc.text(x, y, `SlideId : ${comment.slideid}`);
-      y+=20;
-      doc.text(x, y, `Time : ${comment.updatedAt}`);
-      y+=20;
-      doc.text(x,y,'');
-      y+=20;
-    })
-    doc.setFont("helvetica");
-    doc.save("sample-file.pdf");
-
-  })
   };
   const _getComments = () => {
     return comments.map((comment) => {
@@ -99,7 +75,6 @@ const CommentBox = ({ pptname, pageNumber }) => {
 
   return (
     <>
-      <button onClick={test}>Test</button>
       <div className="comment-box">
         <h2>Add Comments</h2>
         <CommentForm addComment={_addComment} />
